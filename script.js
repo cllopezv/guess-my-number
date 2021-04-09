96cd6c9510bb01aecf1cf2109a19626248a73cd5
Carmen Lopez <80540774+cllopezv@users.noreply.github.com>
2021-04-09 13:29:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highScore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const changeBgColor = color => {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+const changeScoreMsg = score => {
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When there is not input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
+    // When player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = 'Correct Number 🎉';
+    displayMessage('Correct Number 🎉');
+    changeBgColor('#60b347');
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+    score--;
+    changeScoreMsg(score);
+  } else {
+    displayMessage('💥 You lost the game...');
+    changeScoreMsg(0);
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  displayMessage('Start guessing....');
+  changeScoreMsg(score);
+
+  document.querySelector('.guess').value = '';
+
+  changeBgColor('#222');
+  document.querySelector('.number').style.width = '15rem';
+});
